136424802b599d013ffac6740bdc9498b5ddb8be
James Lu <james@overdrivenetworks.com>
2017-01-23 13:59:26 -0800

    Rename PlayerMovement scripts to Player
    These are eventually going to contain way more tracking than just
    movement, so the name should be something that fits.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     public float moveSpeed;
     public float jumpStrength;
