11058ab1a1f6557fe0a45c967cf7e65f1c74eace
James Lu <james@overdrivenetworks.com>
2017-02-02 09:11:05 -0800

    Collidable: remove dead code

--- Assets/Collidable.cs
+++ Assets/Collidable.cs
@@ -4,26 +4,4 @@ using UnityEngine;
 public abstract class Collidable : MonoBehaviour {
     // This will be overriden by inheriting classes.
     public abstract void PlayerHit(Player player);
-/*
-    // This method passes player hit calls to the class representing
-    // an object, based on its tag.
-    void PlayerHit(Player player)
-    {
-        if (string.IsNullOrEmpty(tag)) // No tag on the object; break
-        {
-            return;
-        }
-        Type type = Type.GetType(tag);
-        if (type == null) // Unknown tag, return.
-        {
-            Debug.LogWarning(string.Format("Collidable got null type from tag {0}", tag));
-            return;
-        }
-        dynamic instance = GetComponent(type);
-        if (instance != null)
-        {
-            instance.PlayerHit(player);
-        }
-    }
-*/
 }
