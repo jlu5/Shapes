d2db780fc9dc155888a1c2ff92c008505cc037fa
James Lu <james@overdrivenetworks.com>
2017-03-01 22:34:08 -0800

    PlayerOverlay: show player ID as text in the hotbar

--- Assets/PlayerOverlay.cs
+++ Assets/PlayerOverlay.cs
@@ -4,13 +4,27 @@ using UnityEngine.UI;
 public class PlayerOverlay : MonoBehaviour
 {
     public int playerID;
-
+    private GameObject HUDTextLabelTemplate;
+    private GameObject label;
     private Button button;
 
     void Start()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(OnClick);
+
+        // Label which player this overlay corresponds to
+		HUDTextLabelTemplate = Resources.Load<GameObject>("HUDTextLabel");
+        label = Instantiate(HUDTextLabelTemplate);
+        Text labeltext = label.GetComponent<Text>();
+        labeltext.text = playerID.ToString();
+
+        // Use the same colour as the player object's overlay (for consistency)
+        labeltext.color = GameState.Instance.getPlayer(playerID).textLabelColor;
+
+        // Move the label under the player overlay
+        label.transform.SetParent(transform);
+
     }
 
     public void OnClick()
