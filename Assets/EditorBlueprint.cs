1d0272d869e289fc8369162d12a7c0a116156fdf
James Lu <james@overdrivenetworks.com>
2017-04-19 10:21:11 -0700

    Editor: implement object deletion

---
+++ Assets/EditorBlueprint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorBlueprint : MonoBehaviour {
+
+    void OnMouseUpAsButton()
+    {
+        string currentTool = Editor.Instance.currentObject;
+        if (currentTool == "delete")
+        {
+            Debug.Log("Deleting object " + gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+}
