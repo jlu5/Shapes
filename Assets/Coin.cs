0b865bef30dab49ae3d6b5eabadc2160ee6006c9
James Lu <james@overdrivenetworks.com>
2017-04-27 11:26:44 -0700

    Add coins to increase the score

---
+++ Assets/Coin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Coin : Collidable {
+    // How much is this coin worth?
+    public int value = 100;
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        // When a player hits the coin, increment the score and destroy the coin.
+        if (other.gameObject.GetComponent<Player>() != null)
+        {
+            GameState.Instance.AddScore(value);
+            Destroy(gameObject);
+        }
+    }
+}
