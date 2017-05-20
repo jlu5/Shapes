f4ee6d2d2ec5d2799c556c1cb641972975e9f458
James Lu <james@overdrivenetworks.com>
2017-05-19 19:33:00 -0700

    Add jump powerup

---
+++ Assets/PowerupJump.cs
@@ -0,0 +1,15 @@
+﻿public class PowerupJump : Powerup {
+    public float jumpMultiplier = 1.25F;
+
+    public override void SetEffect()
+    {
+        // TODO: consider changing jumpRecoilStrength as well?
+        targetPlayer.jumpStrength *= jumpMultiplier;
+    }
+
+    public override void RemoveEffect()
+    {
+        base.RemoveEffect();
+        targetPlayer.jumpStrength /= jumpMultiplier;
+    }
+}
