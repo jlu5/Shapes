c025fd3f34aacbe45ccb5317cab310f1cf394c7f
James Lu <james@overdrivenetworks.com>
2017-05-20 14:30:52 -0700

    PowerupGrow: also add a mass multiplier

--- Assets/PowerupGrow.cs
+++ Assets/PowerupGrow.cs
@@ -1,14 +1,17 @@
 ﻿public class PowerupGrow : Powerup {
     public float sizeMultiplier = 1.25F;
+    public float massMultiplier = 2F;
 
     public override void SetEffect()
     {
         targetPlayer.gameObject.transform.localScale *= sizeMultiplier;
+        targetPlayer.rb.mass *= massMultiplier;
     }
 
     public override void RemoveEffect()
     {
         base.RemoveEffect();
         targetPlayer.gameObject.transform.localScale /= sizeMultiplier;
+        targetPlayer.rb.mass /= massMultiplier;
     }
 }
