911684c54f3ae5a2b10faebf5f24c7ee34228d4d
James Lu <james@overdrivenetworks.com>
2017-05-24 10:46:06 -0700

    Add a low gravity powerup

---
+++ Assets/PowerupFeather.cs
@@ -0,0 +1,16 @@
+﻿public class PowerupFeather : Powerup
+{
+    public float gravityMultiplier = 0.5F;
+
+    public override void SetEffect()
+    {
+        targetPlayer.rb.gravityScale *= gravityMultiplier;
+        // TODO: show some graphic?
+    }
+
+    public override void RemoveEffect()
+    {
+        base.RemoveEffect();
+        targetPlayer.rb.gravityScale /= gravityMultiplier;
+    }
+}
