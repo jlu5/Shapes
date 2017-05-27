7e66a3656ef851db652e27a6a7fd0378801f24a2
James Lu <james@overdrivenetworks.com>
2017-05-27 01:57:02 -0700

    Force-include "Particles/Alpha Blended Premultiply" in asset bundles
    This fixes rocket particles going missing when starting from the level selector.

--- Assets/Editor/BuildAssetBundles.cs
+++ Assets/Editor/BuildAssetBundles.cs
@@ -6,7 +6,9 @@ public class CreateAssetBundles : MonoBehaviour
     [MenuItem("Assets/Build Asset Bundles")]
     static void BuildAllAssetBundles()
     {
+        // Clear the cache, and then rebuild the asset bundles.
+        Caching.CleanCache();
         BuildPipeline.BuildAssetBundles("Assets/StreamingAssets/LevelAssetBundles", BuildAssetBundleOptions.None,
-                                        BuildTarget.StandaloneWindows);
+                                        BuildTarget.StandaloneWindows);  // XXX: hardcoding for specific OS
     }
 }
