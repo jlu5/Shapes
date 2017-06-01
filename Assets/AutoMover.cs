3a080965161c678fa9c03ec959cb8a10314bfd2b
James Lu <james@overdrivenetworks.com>
2017-05-31 21:36:17 -0700

    Rewrite AutoMover, which now animates X and Y components in one

--- Assets/AutoMover.cs
+++ Assets/AutoMover.cs
@@ -1,47 +1,79 @@
 ﻿using UnityEngine;
+using System;
 
-public enum AutoMoverValue { positionx, positiony }
+public enum AutoMoverValue { positionX, positionY }
 
 public class AutoMover : MonoBehaviour
 {
-    public AutoMoverValue targetValue;
-    public float startValue;
-    public float endValue;
+
+    // Defines the start and end position values for the target.
+    //public float startX;
+    public float endX;
+    //public float startY;
+    public float endY;
+
+    // Determines how long the animation should last in one direction.
     public float animationLength;
 
+    /*
+    // Fetch Unity's internal animation variable given the friendly value.
     public string GetAnimationAttribute(AutoMoverValue value)
     {
         switch (value)
         {
             case (AutoMoverValue.positionx):
-                return "m_LocalPosition.x";
+                return "localPosition.x";
             case (AutoMoverValue.positiony):
-                return "m_LocalPosition.y";
+                return "localPosition.y";
         }
         return null;
+    }
+    */
+
+    // Fetch the starting animation value (e.g. the X or Y position) given the value name.
+    public float GetStartValue(AutoMoverValue value)
+    {
+        switch (value)
+        {
+            case (AutoMoverValue.positionX):
+                return transform.position.x;
+            case (AutoMoverValue.positionY):
+                return transform.position.y;
+        }
+        throw new ArgumentException("Unknown value " + value.ToString());
     }
 
     void Start()
     {
-
-        //Animation anim = GetComponent<Animation>();
+        // Retrieve the animator and create an override for it.
         Animator anim = GetComponent<Animator>();
-
         AnimatorOverrideController controller = new AnimatorOverrideController(anim.runtimeAnimatorController);
         anim.runtimeAnimatorController = controller;
 
-        // First value: start value, second value: start time, third: animation length, fourth: end value
-        // https://docs.unity3d.com/ScriptReference/AnimationCurve.Linear.html
-        AnimationCurve curve = AnimationCurve.Linear(startValue, 0, animationLength, endValue);
-        AnimationClip clip = new AnimationClip();
-        //clip.legacy = true;
+        // Fetch the starting X and Y values based off the object's initial position.
+        float startX = GetStartValue(AutoMoverValue.positionX);
+        float startY = GetStartValue(AutoMoverValue.positionY);
 
-        string attributeName = GetAnimationAttribute(targetValue);
-        //Keyframe keyNew = new Keyframe(5, 10f);
-        clip.SetCurve("", typeof(Transform), attributeName, curve);
-        //anim.AddClip(clip, "AutoMover");
-        controller["movement"] = clip;
-        anim.Play("movement");
-        //anim.StartPlayback();
+        // Create X and Y coordinate animation curves.
+        // First argument: start time, second argument: start value, third: animation length, fourth: end value
+        // https://docs.unity3d.com/ScriptReference/AnimationCurve.Linear.html
+        AnimationCurve Xcurve = AnimationCurve.Linear(0, startX, animationLength, endX);
+        AnimationCurve Ycurve = AnimationCurve.Linear(0, startY, animationLength, endY);
+        AnimationClip clip = new AnimationClip();
+
+        /* Use the animation curve created above to animate the target property.
+         * 
+         * https://docs.unity3d.com/ScriptReference/AnimationClip.SetCurve.html
+         * First argument: the game object name to apply to (blank means the current object),
+         * Second: the type of component to animate; in this case, it is the object's Transform
+         * Third: the Unity internal attribute name (hardcoded)
+         * Fourth: the animation curve
+         */
+        clip.SetCurve("", typeof(Transform), "localPosition.x", Xcurve);
+        clip.SetCurve("", typeof(Transform), "localPosition.y", Ycurve);
+
+        // Replace the dummy animation placeholders in the animation controller, and play.
+        controller["AutoMoverPlaceholder"] = clip;
+        anim.Play("AutoMover");
     }
 }
