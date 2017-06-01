b592641e805eb60edc77251beb9c7a7f11c9f906
James Lu <james@overdrivenetworks.com>
2017-05-31 22:43:56 -0700

    Add a sticky (high friction) material
    AutoMaterial uses brown for this color.

--- Assets/AutoMaterial.cs
+++ Assets/AutoMaterial.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoMaterial : MonoBehaviour {
+    // Configurable options: whether the object is bouncy, icy, or sticky. Only one of these should be enabled at a time..
     public bool isBouncy;
     public bool isIce;
+    public bool isSticky;
+
+    // Toggles automatic color settings.
     public bool enableColor = true;
+
+    // Misc state tracking
     private bool movable;
     private Color color;
 
     // Material definitions
     private PhysicsMaterial2D materialBouncy;
     private PhysicsMaterial2D materialIce;
+    private PhysicsMaterial2D materialSticky;
 
-    // Color definitions. TODO: describe how they're used
+    // Color definitions. 
     protected Color staticColor;
     protected Color dynamicColor;
     protected Color staticBouncyColor;
     protected Color dynamicBouncyColor;
     protected Color staticIceColor;
     protected Color dynamicIceColor;
+    protected Color stickyColor;
 
     // I personally prefer working with hex colors over rgb values, because they're easier to tweak/replace.
+    // See Run() below for a rough description of what each color corresponds to.
     void Awake() {
         staticColor = Utils.HexColor("#FFFFFF");
         dynamicColor = Utils.HexColor("#FF955C");
@@ -29,9 +38,11 @@ public class AutoMaterial : MonoBehaviour {
         dynamicBouncyColor = Utils.HexColor("#94FFB8");
         staticIceColor = Utils.HexColor("#C9FDFF");
         dynamicIceColor = Utils.HexColor("#9EE3EF");
+        stickyColor = Utils.HexColor("#A18454");
 
         materialBouncy = Resources.Load<PhysicsMaterial2D>("materialBouncy");
         materialIce = Resources.Load<PhysicsMaterial2D>("materialIce");
+        materialSticky = Resources.Load<PhysicsMaterial2D>("materialSticky");
     }
 
     bool IsDynamic() {
@@ -50,22 +61,32 @@ public class AutoMaterial : MonoBehaviour {
 
         movable = IsDynamic();
         Collider2D collider = GetComponent<Collider2D>();
+
         // If the bouncy attribute is set or the object already has bouncy attributes, update the color + material.
+        // Bouncy and static = yellow, bouncy and movable = green
         if (isBouncy || (collider.sharedMaterial == materialBouncy))
         {
             color = movable ? dynamicBouncyColor : staticBouncyColor;
             collider.sharedMaterial = materialBouncy;
         }
-        // Ditto with ice
+
+        // Ditto with ice; both static and dynamic objects are light blue
         else if (isIce || (collider.sharedMaterial == materialIce))
         {
             color = movable ? dynamicIceColor : staticIceColor;
             collider.sharedMaterial = materialIce;
-        }
-        else
-        {
+
+        // Sticky walls are a muddy shade of brown.
+        } else if (isSticky || (collider.sharedMaterial == materialSticky)) {
+            color = stickyColor;
+            collider.sharedMaterial = materialSticky;
+
+        } else
+        { // For regular objects, use the generic white color for static walls and orange for moving objects.
             color = movable ? dynamicColor : staticColor;
         }
+
+        // Automatic colour updating is optional!
         if (enableColor)
         {
             GetComponent<SpriteRenderer>().color = color;
