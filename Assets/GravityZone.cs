af14aa160b68e0107620d73134c1dd5d216947a7
James Lu <james@overdrivenetworks.com>
2017-06-02 00:00:03 -0700

    Add experimental gravity zones

---
+++ Assets/GravityZone.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GravityZone : Collidable {
+    // How much should we multiple objects' gravity by as they enter?
+    public float gravityMultiplier = 0.5f;
+
+    // In case we want to cancel out gravity completely, keep track of objects' previous gravity
+    // values - this is because we can't divide by 0 to restore the old value.
+    //private Dictionary<GameObject, float> previousGravityScales = new Dictionary<GameObject, float>();
+
+    void Start() {
+        if (gravityMultiplier == 0)
+        {
+            Debug.LogError("Removing GravityZone with invalid gravity multiplier of 0");
+            Destroy(this);
+        }
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.attachedRigidbody)
+        {
+            // For gravity multipliers > 0, just multiply the gravity scale by the multiplier.
+            Debug.Log("Object " + other.gameObject.name + " entered gravity zone");
+            other.attachedRigidbody.gravityScale *= gravityMultiplier;
+        }
+    }
+
+    protected override void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.attachedRigidbody)
+        {
+            Debug.Log("Object " + other.gameObject.name + " left gravity zone");
+            other.attachedRigidbody.gravityScale /= gravityMultiplier;
+        }
+    }
+}
