d045d4a0a7de860a8071de920af2447529b34454
James Lu <james@overdrivenetworks.com>
2017-06-03 19:26:55 -0700

    Hack in zero-gravity gravity zones

--- Assets/GravityZone.cs
+++ Assets/GravityZone.cs
@@ -10,14 +10,14 @@ public class GravityZone : Collidable {
     //private Dictionary<GameObject, float> previousGravityScales = new Dictionary<GameObject, float>();
 
     void Start() {
-        if (gravityMultiplier == 0)
-        {
-            Debug.LogError("Removing GravityZone with invalid gravity multiplier of 0");
-            Destroy(this);
-        }
-
         // Make sure the collider matches the size of the object after tiling.
         GetComponent<BoxCollider2D>().size = GetComponent<SpriteRenderer>().size;
+
+        if (gravityMultiplier == 0)
+        {
+            // We can't divide by zero, so set the multiplier to a really low value to simulate lack of gravity.
+            gravityMultiplier = -1e-32f;
+        }
     }
 
     protected override void OnTriggerEnter2D(Collider2D other)
@@ -26,6 +26,17 @@ public class GravityZone : Collidable {
         {
             // For gravity multipliers > 0, just multiply the gravity scale by the multiplier.
             Debug.Log("Object " + other.gameObject.name + " entered gravity zone");
+
+            /*
+            // If the gravity multiplier is 0, we have to use a different method of storing objects' last gravity
+            // scale as they enter and leave the area.
+            if (!previousGravityScales.ContainsKey(other.gameObject))
+            {
+                previousGravityScales[other.gameObject] = other.attachedRigidbody.gravityScale;
+            }
+            */
+
+            // Multiply the target object's gravity scale by the multiplier.
             other.attachedRigidbody.gravityScale *= gravityMultiplier;
         }
     }
@@ -35,7 +46,21 @@ public class GravityZone : Collidable {
         if (other.attachedRigidbody)
         {
             Debug.Log("Object " + other.gameObject.name + " left gravity zone");
+
+            /*
+            if (other.attachedRigidbody.gravityScale == 0 || gravityMultiplier == 0)
+            {
+                // Reset the target object from 0 gravity to its original gravity scale.
+                other.attachedRigidbody.gravityScale = defaultGravityScale;
+            }
+            else
+            {
+                // For multipliers != 0, we can just divide by the multiplier.
+                other.attachedRigidbody.gravityScale /= gravityMultiplier;
+            }
+            */
             other.attachedRigidbody.gravityScale /= gravityMultiplier;
         }
+        //previousGravityScales.Remove(other.gameObject);
     }
 }
