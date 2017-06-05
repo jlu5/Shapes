f237290ad3f57a7f443deaf57fd8ed0879db320d
James Lu <james@overdrivenetworks.com>
2017-06-05 11:22:39 -0700

    Added game boundaries - objects falling past a certain point are automatically destroyed

---
+++ Assets/GameBoundary.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameBoundary : MonoBehaviour {
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            // Destroy players properly
+            GameState.Instance.RemovePlayer(player.playerID);
+        }
+
+        if (other.attachedRigidbody)
+        {
+            // For most objects, destroy them completely.
+            Destroy(other.gameObject);
+        }
+    }
+}
