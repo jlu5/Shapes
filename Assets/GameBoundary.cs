2e6537a7816a35b0c8f02b3dec869ae7f0fec163
James Lu <james@overdrivenetworks.com>
2017-06-07 10:29:52 -0700

    GameBoundary: fix conflict with powerups when they disable their collider
    Only destroy objects if they're actually leaving the area of the game boundary.

--- Assets/GameBoundary.cs
+++ Assets/GameBoundary.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 
 public class GameBoundary : MonoBehaviour {
+    private Collider2D col;
+
+    void Awake()
+    {
+        col = GetComponent<Collider2D>();
+    }
 
     void OnTriggerExit2D(Collider2D other)
     {
         Player player = other.gameObject.GetComponent<Player>();
         if (player)
         {
-            // Destroy players properly
+            // Destroy players via GameState, unregistering their overlay etc.
+            Debug.Log("GameBoundary: removing player " + player.playerID);
             GameState.Instance.RemovePlayer(player.playerID);
         }
 
-        if (other.attachedRigidbody)
+        // Note: Only destroy objects that are *actually* leaving the area of the stage.
+        // In other words, ignore objects that disable their collider intentionally (e.g. powerups when hit).
+        if (other.attachedRigidbody && !col.OverlapPoint(other.transform.position))
         {
             // For most objects, destroy them completely.
+            Debug.Log("GameBoundary: removing object " + other.gameObject.name);
             Destroy(other.gameObject);
         }
     }
