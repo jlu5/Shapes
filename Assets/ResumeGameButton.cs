fdefa79793d2ccafa2334bb11af3779c5772328b
James Lu <james@overdrivenetworks.com>
2017-06-09 16:59:35 -0700

    Oops, forgot to commit this

---
+++ Assets/ResumeGameButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ResumeGameButton : ClickableOverlay
+{
+    public override void OnClick()
+    {
+        Time.timeScale = GameState.Instance.timeScale;
+        LevelSelector.Instance.SwitchCanvas(-1);  // Hide the navigation menu
+    }
+}
