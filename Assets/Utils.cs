407d24fef0de4a9bd8e49475c95da6055cde1cb7
James Lu <james@overdrivenetworks.com>
2017-06-09 18:28:29 -0700

    Move Explode() from Bomb to Utils

--- Assets/Utils.cs
+++ Assets/Utils.cs
@@ -55,4 +55,40 @@ public static class Utils {
             meshText.text = text;
         }
     }
+
+    // Adds an explosion force centered around GameObject with the given explosion radius and max force.
+    // This method returns the amount of objects affected.
+    public static int AddExplosionForce2D(GameObject gobject, float explosionRadius, float explosionForce)
+    {
+        // Basically what we need to do is find a list of objects close to the bomb when it explodes,
+        // and propel them away. The closer the object is to the bomb, the more force is applied on it.
+        Collider2D[] colliderResults = Physics2D.OverlapCircleAll(gobject.transform.position, explosionRadius);
+        foreach (Collider2D collider in colliderResults)
+        {
+            if (collider.attachedRigidbody)
+            {
+                // Calculate the difference between the target object and the bomb's positions.
+                Vector2 differenceVector = collider.transform.position - gobject.transform.position;
+                float distance = differenceVector.magnitude;
+
+                // Calculate a force amount inversely proportional to the distance betwen the two objects.
+                float force = Mathf.Min(1 / distance, distance) * explosionForce;
+
+                // Fetch the angle between the bomb and the target object.
+                float explosionAngle = Mathf.Atan2(differenceVector.y, differenceVector.x);
+
+                // Create a new vector based off this angle.
+                Vector2 targetVector = new Vector2(force * Mathf.Cos(explosionAngle), force * Mathf.Sin(explosionAngle));
+
+                collider.attachedRigidbody.AddForce(targetVector, ForceMode2D.Impulse);
+
+                Debug.Log("Difference from object " + collider.gameObject.name + " to bomb: " + differenceVector.ToString());
+                Debug.Log("Explosion angle is " + explosionAngle + " radians with object " + collider.gameObject.name);
+                Debug.Log("cos(" + explosionAngle.ToString() + ") is " + Mathf.Cos(explosionAngle).ToString());
+                Debug.Log("Using force " + force.ToString() + " on object " + collider.gameObject.name);
+                Debug.Log("Adding force of magnitude " + targetVector.ToString() + " to object " + collider.gameObject.name);
+            }
+        }
+        return colliderResults.Length;
+    }
 }
